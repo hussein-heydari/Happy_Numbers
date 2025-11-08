aa85f69da6e2aea2194eb245794e1abf00ca0dd0
Hussein Heydari <albuspwbd99@gmail.com>
2025-11-08 11:41:55 +0330

    feat: Add the main flow to the code Include an exit option for the user

--- src/main.py
+++ src/main.py
@@ -10,8 +10,19 @@ def is_happy_num(num):
     squared_sum = 0
     for i in iterable_num:
         squared_sum += i ** 2 
-    print(squared_sum)
     if squared_sum == 1:
         return True
     else:
         return is_happy_num(squared_sum)
+
+while True:
+    try:
+        user_input = input("You give me a number, and I'll tell you if it's a happy number or not" \
+                           "(You can type in exit or quit to terminate the program): ")
+        if user_input.lower() in ['exit','quit', 'q']:
+            print("Exiting the program. Goodbye!")
+            break
+        if is_happy_num(user_input):
+            print("This number is a happy number!")
+    except RecursionError:
+        print("This number is not a happy number.")
